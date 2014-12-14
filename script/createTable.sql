64873bd5ed1258f3d76fca8f92295aad3777d29c
WardenQQ <WardenQQ@server.fake>
2014-12-14 19:12:06 +0100

    Table Bien et Service.

--- script/createTable.sql
+++ script/createTable.sql
@@ -1,22 +1,60 @@
 CREATE TABLE Adresse(
-  rue VARCHAR2 NOT NULL,
-  ville VARCHAR2 NOT NULL,
-  region VARCHAR2 NOT NULL,
+  rue VARCHAR2(256) NOT NULL,
+  ville VARCHAR2(256) NOT NULL,
+  region VARCHAR2(256) NOT NULL,
   latitude NUMBER,
   longitude NUMBER,
-
-  CONSTRAINT Adress_PK PRIMARY KEY (rue, ville, region)
+  PRIMARY KEY (rue, ville, region)
 );
 
 CREATE TABLE Utilisateur(
-  login VARCHAR2 NOT NULL,
-  motDePasse VARCHAR2 NOT NULL,
-  rue VARCHAR2 NOT NULL,
-  ville VARCHAR2 NOT NULL,
-  region VARCHAR2 NOT NULL,
-  urlPhoto VARCHAR2 NOT NULL,
-
-  CONSTRAINT Utilisateur_PK PRIMARY KEY (login)
-  CONSTRAINT Adresse_FK FOREIGN KEY (rue, ville, region)
+  login VARCHAR2(32) NOT NULL,
+  motDePasse VARCHAR2(32) NOT NULL,
+  rue VARCHAR2(256) NOT NULL,
+  ville VARCHAR2(256) NOT NULL,
+  region VARCHAR2(256) NOT NULL,
+  urlPhoto VARCHAR2(256),
+  PRIMARY KEY (login),
+  FOREIGN KEY (rue, ville, region)
     REFERENCES Adresse(rue, ville, region)
 );
+
+CREATE TABLE Categorie(
+  nom VARCHAR(32),
+  parent VARCHAR(32),
+  PRIMARY KEY (nom),
+  FOREIGN KEY (parent)
+    REFERENCES Categorie(nom)
+);
+
+CREATE TABLE Bien(
+  proprietaire VARCHAR2(32) NOT NULL,
+  nom VARCHAR2(32) NOT NULL,
+  categorie VARCHAR2(32),
+  etat NUMBER(1) NOT NULL,
+  valeurEstimee NUMBER DEFAULT(0) NOT NULL,
+  descriptif VARCHAR2(32),
+  urlPhoto VARCHAR2(32),
+  PRIMARY KEY (proprietaire, nom, categorie, etat),
+  FOREIGN KEY (proprietaire)
+    REFERENCES Utilisateur(login),
+  FOREIGN KEY (categorie)
+    REFERENCES Categorie(nom)
+);
+
+CREATE TABLE Service(
+  proprietaire VARCHAR2(32) NOT NULL,
+  nom VARCHAR2(32) NOT NULL,
+  categorie VARCHAR2(32),
+  dateService DATE NOT NULL,
+  valeurEstimee NUMBER DEFAULT(0) NOT NULL,
+  descriptif VARCHAR2(32),
+  urlPhoto VARCHAR2(32),
+  PRIMARY KEY (proprietaire, nom, categorie, dateService),
+  FOREIGN KEY (proprietaire)
+    REFERENCES Utilisateur(login),
+  FOREIGN KEY (categorie)
+    REFERENCES Categorie(nom)
+);
+
+
--- script/dropTable.sql
+++ script/dropTable.sql
@@ -1,2 +1,5 @@
+DROP TABLE Service;
+DROP TABLE Bien;
+DROP TABLE Categorie;
 DROP TABLE Utilisateur;
 DROP TABLE Adresse;
